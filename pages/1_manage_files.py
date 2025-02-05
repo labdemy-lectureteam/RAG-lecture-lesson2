66d437fab0da9ba73c949c42552d68563e37a71a
labdemy-lectureteam <lecture@labdemy.com>
2025-02-05 19:13:58 +0900

    added file removal

--- pages/1_manage_files.py
+++ pages/1_manage_files.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-from streamlit_js_eval import streamlit_js_eval
 from app import reset_st_session
 
 st.write('## ファイル管理画面')
@@ -14,3 +13,18 @@ if uploaded_files := st.file_uploader("ファイルをアップロード：",acc
     reset_st_session()    
     msg = 'PDFデータの更新が完了しました。'
     st.session_state.messages.append({'role':'assistant', 'content':msg})
+
+
+st.write('#### ファイル一覧')
+files_to_remove = []
+if os.path.isdir('documents') and len(os.listdir('documents')) != 0:
+    for file_name in os.listdir('documents'):
+        remove_file = st.checkbox(file_name)
+        if remove_file:
+            files_to_remove.append(file_name)
+
+if st.button("選択したファイルを削除",key='delete_files'):
+    for file in files_to_remove:
+        os.remove(f'documents/{file}')
+    st.rerun()
+    
