50a564a45710be416e0417e5081ab1813072b611
labdemy-lectureteam <lecture@labdemy.com>
2025-02-05 17:30:34 +0900

    added chat history

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
+from typing import List, Dict
 
 load_dotenv()  # .envファイルからAPIキーを呼び出す。
 
@@ -15,6 +17,18 @@ if "session_reset_done" not in st.session_state.keys():
     reset_st_session()
     st.session_state.session_reset_done = True
     
+def format_prompt(query:str, chat_history:List[Dict[str, str]])->str:
+    PROMPT = """
+    You are a helpful assistant. Answer the user given the chat history:
+    
+    chat history: {CHAT_HISTORY}
+    user query: {QUERY}
+    """
+    prompt = ChatPromptTemplate.from_template(PROMPT)
+    chat_history = '\n\n'.join([f"{message['role']}: {message['content']}" for message in chat_history])
+    prompt = prompt.format(CHAT_HISTORY=chat_history,QUERY=query)
+    return prompt
+    
 for message in st.session_state.messages:
     with st.chat_message(message['role']):
         st.markdown(message['content'])
@@ -24,6 +38,8 @@ if user_prompt := st.chat_input():
         st.markdown(user_prompt)
     st.session_state.messages.append({'role':'user', 'content':user_prompt})
     
+    final_prompt = format_prompt(user_prompt,st.session_state.messages)
+    
     with st.chat_message('assistant'):
-        response = st.write_stream(st.session_state.model.stream(user_prompt))
+        response = st.write_stream(st.session_state.model.stream(final_prompt))
     st.session_state.messages.append({'role':'assistant', 'content':response})
