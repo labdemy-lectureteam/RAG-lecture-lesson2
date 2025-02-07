93e4bf9662150e7d0ed68f9fb4b3e3a865ff52b8
labdemy-lectureteam <lecture@labdemy.com>
2025-02-07 13:24:37 +0900

    TODO-ified main scripts

--- app.py
+++ app.py
@@ -5,51 +5,22 @@ from dotenv import load_dotenv
 from typing import List, Dict
 from rag import *
 
-load_dotenv()  # .envファイルからAPIキーを呼び出す。
+# .envファイルからAPIキーを呼び出す。
+#TODO
 
 st.title('RAG チャットボット')
 
-def reset_st_session():
-    if 'messages' not in st.session_state:
-        st.session_state.messages = []
-    st.session_state.model = ChatOpenAI(model='gpt-4o-mini') 
-    try:
-        documents = load_PDF(path='documents')
-    except:
-        st.warning('RAGを使うためにはPDFファイルをアップロードしてください', icon="⚠️")
-    else:
-        chunks = create_chunks(documents=documents, chunk_size=500, chunk_overlap=50)
-        embedding_model= OpenAIEmbeddings(model='text-embedding-3-small')
-        vector_db = init_vector_db(embedding_model=embedding_model,db_path="database")
-        vector_db.add_documents(documents=chunks)
-        st.session_state.vector_db = vector_db
-        msg = 'PDFデータの準備が完了しました。'
-        st.success(msg)
-    return
+#セッションの初期化関数
+#TODO
 
-if "session_reset_done" not in st.session_state.keys():
-    reset_st_session()
-    st.session_state.session_reset_done = True
+#セッションの初期化
+#TODO
 
-    
-for message in st.session_state.messages:
-    with st.chat_message(message['role']):
-        st.markdown(message['content'])
+#チャット履歴に含まれるメッセージの描写
+#TODO
         
-user_prompt = st.chat_input()
-if user_prompt:
-    with st.chat_message('user'):
-        st.markdown(user_prompt)
-    st.session_state.messages.append({'role':'user', 'content':user_prompt})
-    
-    # Retreivalとprompt整形
-    context = get_context_from_db(st.session_state.vector_db, user_prompt) 
-    final_prompt,sources = format_prompt(context, user_prompt,st.session_state.messages)
-    
-    # 回答と出典の表示
-    with st.chat_message('assistant'):
-        response = st.write_stream(st.session_state.model.stream(final_prompt))
-        formatted_sources = '\n 参考文献：\n' 
-        formatted_sources += '\n '.join(f"{idx+1}. {source['source']}, {source['page']}ページ" for idx,source in enumerate(sources))
-        st.markdown(formatted_sources)
-    st.session_state.messages.append({'role':'assistant', 'content':response+formatted_sources})
+#ユーザー入力の受け取りと表示
+#TODO
+
+# user_promptがあった場合の処理
+#TODO
--- pages/1_manage_files.py
+++ pages/1_manage_files.py
@@ -5,29 +5,10 @@ from streamlit_js_eval import streamlit_js_eval
 
 st.write('## ファイル管理画面')
 
-uploaded_files = st.file_uploader("ファイルをアップロード：",accept_multiple_files=True)
-if uploaded_files:
-    if not os.path.exists("documents"):
-        os.makedirs("documents")
-    for uploaded_file in uploaded_files:
-        with open(os.path.join("documents",uploaded_file.name),"wb") as f:
-            f.write(uploaded_file.getbuffer())
-    reset_st_session()    
-    msg = 'PDFデータの更新が完了しました。'
-    st.session_state.messages.append({'role':'assistant', 'content':msg})
-    streamlit_js_eval(js_expressions="parent.window.location.reload()")
+# ファイルのアップロードモジュール（複数ファイルを受け付けるようにする）
+
+# ファイルがアップロードされた場合の処理
 
 st.write('#### ファイル一覧')
-files_to_remove = []
-if os.path.isdir('documents') and len(os.listdir('documents')) != 0:
-    for file_name in os.listdir('documents'):
-        remove_file = st.checkbox(file_name)
-        if remove_file:
-            files_to_remove.append(file_name)
 
-if st.button("選択したファイルを削除",key='delete_files'):
-    for file in files_to_remove:
-        os.remove(f'documents/{file}')
-    st.rerun()
-    # streamlit_js_eval(js_expressions="parent.window.location.reload()")
-    
+# ファイルの削除処理    
